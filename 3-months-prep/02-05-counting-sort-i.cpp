cfeeb15dafab59210494dc92dd37a913a491cc55
Matthew L Davis <mld.0@protonmail.com>
2021-12-06 20:19:16 +1100

    counting sort in C++

---
+++ 3-months-prep/02-05-counting-sort-i.cpp
@@ -0,0 +1,118 @@
+//	VIM SETTINGS: {{{3
+//	vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+//	vim: set foldlevel=2 foldcolumn=2:
+//	}}}1
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <map>
+#include <climits>
+#include <cassert>
+using namespace std;
+//	{{{2
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &, const char);
+template <typename T>
+string join(const vector<T>, const char);
+template <typename T>
+void printVector(string varname, const vector<T> words, const char delim=' ');
+
+
+vector<int> countingSort(vector<int> arr)
+{
+	vector<int> result(100, 0);
+	for (const auto &x: arr) {
+		result.at(x) += 1;
+	}
+	return result;
+}
+
+
+int main()
+{
+	vector<vector<int>> input_values = { {63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33}, };
+	vector<vector<int>> input_checks = { {0, 2, 0, 2, 0, 0, 1, 0, 1, 2, 1, 0, 1, 1, 0, 0, 2, 0, 1, 0, 1, 2, 1, 1, 1, 3, 0, 2, 0, 0, 2, 0, 3, 3, 1, 0, 0, 0, 0, 2, 2, 1, 1, 1, 2, 0, 2, 0, 1, 0, 1, 0, 0, 1, 0, 0, 2, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 2, 1, 3, 2, 0, 0, 2, 1, 2, 1, 0, 2, 2, 1, 2, 1, 2, 1, 1, 2, 2, 0, 3, 2, 1, 1, 0, 1, 1, 1, 0, 2, 2}, };
+	assert (input_values.size() == input_checks.size());
+	
+	for (int i = 0; i < input_values.size(); ++i) {
+		vector<int> &loop_values = input_values[i];
+		vector<int> &loop_check = input_checks[i];
+
+		printVector("loop_values", loop_values);
+		printVector("loop_check", loop_check);
+
+		vector<int> result = countingSort(loop_values);
+		printVector("result", result);
+
+		assert (result == loop_check);
+		cerr << "\n";
+	}
+
+	return 0;
+}
+
+
+string ltrim(const string &str) {
+//	{{{
+    string s(str);
+    auto it = std::find_if(s.begin(), s.end(),
+                    [](char c) {
+                        return !std::isspace<char>(c, std::locale::classic());
+                    });
+    s.erase(s.begin(), it);
+    return s;
+}
+//	}}}
+
+string rtrim(const string &str) {
+//	{{{
+    string s(str);
+    auto it = std::find_if(s.rbegin(), s.rend(),
+                    [](char c) {
+                        return !std::isspace<char>(c, std::locale::classic());
+                    });
+    s.erase(it.base(), s.end());
+    return s;
+}
+//	}}}
+
+vector<string> split(const string &str, const char delim) {
+//	{{{
+    vector<string> tokens;
+    string::size_type start = 0;
+    string::size_type end = 0;
+    //while ((end = str.find(" ", start)) != string::npos) {
+    while ((end = str.find(delim, start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + 1;
+    }
+    tokens.push_back(str.substr(start));
+    return tokens;
+}
+//	}}}
+
+template <typename T>
+string join(const vector<T> words, const char delim) {
+	//	{{{
+	string result = "";
+	for (int i = 0; i < words.size(); ++i) {
+		result += to_string(words[i]);
+		if (i != words.size()-1) { result += delim; }
+	}
+	return result;
+}
+	//	}}}
+
+template <typename T>
+void printVector(string varname, const vector<T> words, const char delim) {
+	//	{{{
+	string vector_str = join(words, delim);
+	cerr << varname << "=(" << vector_str << ")\n";
+}
+	//	}}}
+
+
