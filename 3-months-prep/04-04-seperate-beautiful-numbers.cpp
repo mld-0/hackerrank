a59e8d7c73ab2f236eb51313cbf0a4609ab83672
Matthew L Davis <mld.0@protonmail.com>
2021-12-10 20:51:03 +1100

    recursive checking string manipulation task in C++
    Hints would be substrings, string->number conversion, and startswith.
    Validation of 'beautiful number' iteratively performs the recursive
    check for each possible first number in the string.

---
+++ 3-months-prep/04-04-seperate-beautiful-numbers.cpp
@@ -0,0 +1,153 @@
+//	VIM SETTINGS: {{{3
+//	vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+//	vim: set foldlevel=2 foldcolumn=2:
+//	}}}1
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <climits>
+#include <cassert>
+#include <vector>
+#include <string>
+#include <map>
+#include <tuple>
+#include <set>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+//	{{{2
+//	ltrim(), rtrim(), split(), join(), printVector(), printMap()
+//	{{{
+string ltrim(const string &str) {
+//	{{{
+    string s(str);
+    auto it = std::find_if(s.begin(), s.end(),
+                    [](char c) {
+                        return !std::isspace<char>(c, std::locale::classic());
+                    });
+    s.erase(s.begin(), it);
+    return s;
+}
+//	}}}
+string rtrim(const string &str) {
+//	{{{
+    string s(str);
+    auto it = std::find_if(s.rbegin(), s.rend(),
+                    [](char c) {
+                        return !std::isspace<char>(c, std::locale::classic());
+                    });
+    s.erase(it.base(), s.end());
+    return s;
+}
+//	}}}
+vector<string> split(const string &str, const char delim=' ') {
+//	{{{
+    vector<string> tokens;
+    string::size_type start = 0;
+    string::size_type end = 0;
+    while ((end = str.find(delim, start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + 1;
+    }
+    tokens.push_back(str.substr(start));
+    return tokens;
+}
+//	}}}
+template <typename T>
+string join(const vector<T> words, const char delim=' ') {
+	//	{{{
+	string result = "";
+	for (int i = 0; i < words.size(); ++i) {
+		result += std::to_string(words[i]);
+		if (i != words.size()-1) { result += delim; }
+	}
+	return result;
+}
+	//	}}}
+template <typename T>
+void printVector(string varname, const vector<T> words, const char delim=' ') {
+	//	{{{
+	string vector_str = join(words, delim);
+	cerr << varname << "=(" << vector_str << ")\n";
+}
+	//	}}}
+template <typename A, typename B>
+void printMap(string varname, const map<A,B> m, const char delim=' ') {
+	//	{{{
+	string m_str = "";
+	for (auto const &x: m) {
+		m_str += std::to_string(x.first);
+		m_str += ":";
+		m_str += std::to_string(x.second);
+		m_str += delim;
+	}
+	m_str.pop_back();
+	cerr << varname << "=(" << m_str << ")\n";
+}
+	//	}}}
+//	}}}
+
+bool check_valid(string number, string trailing)
+{
+	if (trailing.size() == 0) {
+		return true;
+	}
+	string next_number = to_string(stol(number) + 1);
+	//	if not trailing.startswith(str(next_number))
+	if (trailing.rfind(next_number,0) != 0) {
+		return false;
+	}
+	string next_trailing = trailing.substr(next_number.size());
+	return check_valid(next_number, next_trailing);
+}
+
+void seperateNumbers(string s)
+{
+	for (int k = 1; k < s.size()-1; ++k) {
+		string leading = s.substr(0, k);
+		string trailing = s.substr(k);
+		if (check_valid(leading, trailing)) {
+			cout << "YES " << leading << "\n";
+			return;
+		}
+	}
+	cout << "NO\n";
+}
+
+
+int main()
+{
+	vector<vector<string>> input_values = { {"1234", "91011", "99100", "101103", "010203", "13", "1"}, {"99910001001", "7891011", "9899100", "999100010001"}, {"1", "2", "33", "4445", "8889", "8910"}, };
+	vector<vector<string>> input_checks = {
+		{"YES 1", "YES 9", "YES 99", "NO", "NO", "NO", "NO"}, {"YES 999", "YES 7", "YES 98", "NO"}, {"NO", "NO", "NO", "YES 44", "YES 88", "YES 8"}, };
+	assert (input_values.size() == input_checks.size());
+
+	for (int i = 0; i < input_values.size(); ++i) {
+		assert (input_values[i].size() == input_checks[i].size());
+
+		//	Redirect cout
+		stringstream ss;
+		streambuf *coutbuf = cout.rdbuf();
+		cout.rdbuf(ss.rdbuf());
+
+		for (int j = 0; j < input_values[i].size(); ++j) {
+			string s = input_values[i][j];
+			cerr << "s=(" << s << ")\n";
+			string check = input_checks[i][j];
+			//cerr << "check=(" << check << ")\n";
+
+			ss.str(string());
+			seperateNumbers(s);
+			string result = rtrim(ss.str());
+			cerr << "result=(" << result << ")\n";
+
+			assert (result == check);
+			cerr << "\n";
+		}
+
+		cout.rdbuf(coutbuf);
+	}
+
+	return 0;
+}
+
