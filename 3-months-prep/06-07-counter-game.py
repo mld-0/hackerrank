05b9ff7ae611cdd5b2cae54002dc1a7c49ed55b9
Matthew L Davis <mld.0@protonmail.com>
2021-12-14 19:05:09 +1100

    cleanup

--- 3-months-prep/06-07-counter-game.py
+++ 3-months-prep/06-07-counter-game.py
@@ -1,32 +1,26 @@
 import math
-import os
-import random
-import re
-import sys
 
-
-    def counterGame(n):
-        count = 0
-        while n > 1:
-            if math.log2(n) == int(math.log2(n)):
-                n = n / 2
-            else:
-                n -= 2 ** int(math.log2(n))
-            count += 1
-        if count % 2 == 0:
-            return "Richard"
+def counterGame(n: int -> str):
+    count = 0
+    while n > 1:
+        if math.log2(n) == int(math.log2(n)):
+            n = n / 2
         else:
-            return "Louise"
+            n -= 2 ** int(math.log2(n))
+        count += 1
+    if count % 2 == 0:
+        return "Richard"
+    else:
+        return "Louise"
 
-if __name__ == '__main__':
-    t = 5
-    input_values = [ 1560834904, 1768820483, 1533726144, 1620434450, 1463674015, ]
-    input_checks = [ "Richard", "Richard", "Louise", "Richard", "Louise", ]
 
-    for n, check in zip(input_values, input_checks):
-        #n = int(input().strip())
-        result = counterGame(n)
-        print("result=(%s)" % result)
-        assert result == check, "Check comparison failed"
-        print()
+input_values = [ 1560834904, 1768820483, 1533726144, 1620434450, 1463674015, ]
+input_checks = [ "Richard", "Richard", "Louise", "Richard", "Louise", ]
+
+for n, check in zip(input_values, input_checks):
+    print("n=(%s)" % n)
+    result = counterGame(n)
+    print("result=(%s)" % result)
+    assert result == check, "Check comparison failed"
+    print()
 
