7c31065114c534f719cd6b9b738e45bdfe9f03e2
Matthew L Davis <mld.0@protonmail.com>
2021-12-14 19:18:49 +1100

    cleanup, add typing

--- 3-months-prep/07-02-new-years-chaos.py
+++ 3-months-prep/07-02-new-years-chaos.py
@@ -1,16 +1,6 @@
-import math
-import os
-import random
-import re
-import sys
+from typing import List, Union
 
-#
-# Complete the 'minimumBribes' function below.
-#
-# The function accepts INTEGER_ARRAY q as parameter.
-#
-
-def minimumBribes(q):
+def minimumBribes(q: List[int]) -> Union[int, bool]:
     #   Update to begin at 0
     queue = [ x-1 for x in q ]
     bribes = 0
@@ -25,15 +15,14 @@ def minimumBribes(q):
     return bribes
 
 
+input_values = [ [2,1,5,3,4,], [2,5,1,3,4,], [5,1,2,3,7,8,6,4], [1,2,5,3,7,8,6,4], ]
+input_checks = [ 3, False, False, 7,]
 
-if __name__ == '__main__':
-    input_values = [ [2, 1, 5, 3, 4,], [2, 5, 1, 3, 4,], list(map(int, "5 1 2 3 7 8 6 4".split())), list(map(int, "1 2 5 3 7 8 6 4".split())), ]
-    input_checks = [ 3, False, False, 7, ]
+for q, check in zip(input_values, input_checks):
+    print("q=(%s)" % q)
+    result = minimumBribes(q)
+    print("result=(%s)" % result)
+    assert result == check, "Check comparison failed"
+    print()
 
-    for q, check in zip(input_values, input_checks):
-        result = minimumBribes(q)
-        print("result=(%s)" % result)
-        assert result == check, "Check comparison failed"
-        print()
-
-        
+    
