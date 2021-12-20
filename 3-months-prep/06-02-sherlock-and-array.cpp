449de9d50a15462dde790efdbc86b22c181bac2e
Matthew L Davis <mld.0@protonmail.com>
2021-12-20 18:17:53 +1000

    vector iteration/sum task in C++

---
+++ 3-months-prep/06-02-sherlock-and-array.cpp
@@ -0,0 +1,167 @@
+//	VIM SETTINGS: {{{3
+//	vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+//	vim: set foldlevel=2 foldcolumn=2:
+//	}}}1
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <climits>
+#include <cassert>
+#include <vector>
+#include <string>
+#include <map>
+#include <tuple>
+#include <set>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+//	{{{2
+//	ltrim(), rtrim(), split(), join(), printVector(), printMap(), print2DVector()
+//	{{{
+string ltrim(const string &str) {
+//	{{{
+    string s(str);
+    auto it = std::find_if(s.begin(), s.end(),
+                    [](char c) {
+                        return !std::isspace<char>(c, std::locale::classic());
+                    });
+    s.erase(s.begin(), it);
+    return s;
+}
+//	}}}
+string rtrim(const string &str) {
+//	{{{
+    string s(str);
+    auto it = std::find_if(s.rbegin(), s.rend(),
+                    [](char c) {
+                        return !std::isspace<char>(c, std::locale::classic());
+                    });
+    s.erase(it.base(), s.end());
+    return s;
+}
+//	}}}
+vector<string> split(const string &str, const char delim=' ') {
+//	{{{
+    vector<string> tokens;
+    string::size_type start = 0;
+    string::size_type end = 0;
+    while ((end = str.find(delim, start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + 1;
+    }
+    tokens.push_back(str.substr(start));
+    return tokens;
+}
+//	}}}
+template <typename T>
+string join(const vector<T> words, const char delim=' ') {
+	//	{{{
+	string result = "";
+	for (int i = 0; i < words.size(); ++i) {
+		result += std::to_string(words[i]);
+		if (i != words.size()-1) { result += delim; }
+	}
+	return result;
+}
+	//	}}}
+string join(const vector<string> words, const char delim=' ') {
+	//	{{{
+	string result = "";
+	for (int i = 0; i < words.size(); ++i) {
+		result += words[i];
+		if (i != words.size()-1) { result += delim; }
+	}
+	return result;
+}
+	//	}}}
+string join(const vector<char> words, const char delim=' ') {
+	//	{{{
+	string result = "";
+	for (int i = 0; i < words.size(); ++i) {
+		result += words[i];
+		if (i != words.size()-1) { result += delim; }
+	}
+	return result;
+}
+	//	}}}
+template <typename T>
+void printVector(string varname, const vector<T> words, const char delim=' ') {
+	//	{{{
+	string vector_str = join(words, delim);
+	cerr << varname << "=(" << vector_str << ")\n";
+}
+	//	}}}
+template <typename T>
+void print2DVector(string varname, const vector<vector<T>> words, const char delim=' ') {
+	//	{{{
+	string result = "(";
+	for (int i = 0; i < words.size(); ++i) {
+		result += "(";
+		string loop_result = join(words[i], ',');
+		result += loop_result;
+		result += ")";
+		if (i != words.size()-1) {
+			result += ", ";
+		}
+	}
+	result += ")\n";
+	cerr << varname << "=" << result;
+}
+	//	}}}
+template <typename A, typename B>
+void printMap(string varname, const map<A,B> m, const char delim=' ') {
+	//	{{{
+	string m_str = "";
+	for (auto const &x: m) {
+		m_str += std::to_string(x.first);
+		m_str += ":";
+		m_str += std::to_string(x.second);
+		m_str += delim;
+	}
+	m_str.pop_back();
+	cerr << varname << "=(" << m_str << ")\n";
+}
+	//	}}}
+//	}}}
+
+string balancedSums(vector<int> arr)
+{
+	if (accumulate(arr.begin()+1, arr.end(), 0) == 0) {
+		return "YES";
+	}
+	if (accumulate(arr.begin(), arr.end()-1, 0) == 0) {
+		return "YES";
+	}
+	int sum_l = 0;
+	int sum_r = accumulate(arr.begin()+1, arr.end(), 0);
+	for (int i = 1; i < arr.size()-1; ++i) {
+		sum_l += arr[i-1];
+		sum_r -= arr[i];
+		if (sum_l == sum_r) {
+			return "YES";
+		}
+	}
+	return "NO";
+}
+
+
+int main()
+{
+	vector<vector<int>> input_values = { {1,2,3}, {1,2,3,3}, {1,1,4,1,1}, {2,0,0,0}, {0,0,2,0}, };
+	vector<string> input_checks = { "NO", "YES", "YES", "YES", "YES", };
+	assert (input_values.size() == input_checks.size());
+
+	for (int i = 0; i < input_values.size(); ++i) {
+		vector<int>& arr = input_values[i];
+		printVector("arr", arr);
+		string check = input_checks[i];
+
+		string result = balancedSums(arr);
+		cerr << "result=(" << result << ")\n";
+		assert (result == check);
+		cerr << "\n";
+	}
+
+	return 0;
+}
+
