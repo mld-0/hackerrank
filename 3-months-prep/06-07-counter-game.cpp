93dd2ebc5b8cd6c62a9e0f29b55a49457f4a0297
Matthew L Davis <mld.0@protonmail.com>
2021-12-21 15:13:30 +1000

    long/double comparison task in C++

---
+++ 3-months-prep/06-07-counter-game.cpp
@@ -0,0 +1,163 @@
+//	VIM SETTINGS: {{{3
+//	vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+//	vim: set foldlevel=2 foldcolumn=2:
+//	}}}1
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <climits>
+#include <cassert>
+#include <vector>
+#include <string>
+#include <map>
+#include <tuple>
+#include <set>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+//	{{{2
+//	ltrim(), rtrim(), split(), join(), printVector(), printMap(), print2DVector()
+//	{{{
+string ltrim(const string &str) {
+//	{{{
+    string s(str);
+    auto it = std::find_if(s.begin(), s.end(),
+                    [](char c) {
+                        return !std::isspace<char>(c, std::locale::classic());
+                    });
+    s.erase(s.begin(), it);
+    return s;
+}
+//	}}}
+string rtrim(const string &str) {
+//	{{{
+    string s(str);
+    auto it = std::find_if(s.rbegin(), s.rend(),
+                    [](char c) {
+                        return !std::isspace<char>(c, std::locale::classic());
+                    });
+    s.erase(it.base(), s.end());
+    return s;
+}
+//	}}}
+vector<string> split(const string &str, const char delim=' ') {
+//	{{{
+    vector<string> tokens;
+    string::size_type start = 0;
+    string::size_type end = 0;
+    while ((end = str.find(delim, start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + 1;
+    }
+    tokens.push_back(str.substr(start));
+    return tokens;
+}
+//	}}}
+template <typename T>
+string join(const vector<T> words, const char delim=' ') {
+	//	{{{
+	string result = "";
+	for (int i = 0; i < words.size(); ++i) {
+		result += std::to_string(words[i]);
+		if (i != words.size()-1) { result += delim; }
+	}
+	return result;
+}
+	//	}}}
+string join(const vector<string> words, const char delim=' ') {
+	//	{{{
+	string result = "";
+	for (int i = 0; i < words.size(); ++i) {
+		result += words[i];
+		if (i != words.size()-1) { result += delim; }
+	}
+	return result;
+}
+	//	}}}
+string join(const vector<char> words, const char delim=' ') {
+	//	{{{
+	string result = "";
+	for (int i = 0; i < words.size(); ++i) {
+		result += words[i];
+		if (i != words.size()-1) { result += delim; }
+	}
+	return result;
+}
+	//	}}}
+template <typename T>
+void printVector(string varname, const vector<T> words, const char delim=' ') {
+	//	{{{
+	string vector_str = join(words, delim);
+	cerr << varname << "=(" << vector_str << ")\n";
+}
+	//	}}}
+template <typename T>
+void print2DVector(string varname, const vector<vector<T>> words, const char delim=' ') {
+	//	{{{
+	string result = "(";
+	for (int i = 0; i < words.size(); ++i) {
+		result += "(";
+		string loop_result = join(words[i], ',');
+		result += loop_result;
+		result += ")";
+		if (i != words.size()-1) {
+			result += ", ";
+		}
+	}
+	result += ")\n";
+	cerr << varname << "=" << result;
+}
+	//	}}}
+template <typename A, typename B>
+void printMap(string varname, const map<A,B> m, const char delim=' ') {
+	//	{{{
+	string m_str = "";
+	for (auto const &x: m) {
+		m_str += std::to_string(x.first);
+		m_str += ":";
+		m_str += std::to_string(x.second);
+		m_str += delim;
+	}
+	m_str.pop_back();
+	cerr << varname << "=(" << m_str << ")\n";
+}
+	//	}}}
+//	}}}
+
+double epsilon = 0.0001;
+
+string counterGame(long n)
+{
+	int count = 0;
+	while (n > 1) {
+		if (abs(log2(n) - floor(log2(n))) < epsilon) {
+			n = n / 2;
+		} else {
+			n -= pow(2, floor(log2(n)));
+		}
+		count++;
+	}
+	if (count % 2 == 0) {
+		return "Richard";
+	} else {
+		return "Louise";
+	}
+}
+
+int main()
+{
+	vector<long> input_values = { 1560834904, 1768820483, 1533726144, 1620434450, 1463674015, 1073741824, 1073741824, 1459730561, 8, 1246326493, 1384145241, 660570904136157, 605846385118282, 562949953421312, 16384, 262144, };
+	vector<string> input_checks = { "Richard", "Richard", "Louise", "Richard", "Louise", "Richard", "Richard", "Louise", "Louise", "Richard", "Richard", "Richard", "Louise", "Louise", "Richard", "Richard", };
+	assert (input_values.size() == input_checks.size());
+
+	for (int i = 0; i < input_values.size(); ++i) {
+		long n = input_values[i];
+		string check = input_checks[i];
+		cerr << "n=(" << n << ")\n";
+		string result = counterGame(n);
+		cerr << "result=(" << result << ")\n";
+		assert (result == check);
+		cerr << "\n";
+	}
+
+}
